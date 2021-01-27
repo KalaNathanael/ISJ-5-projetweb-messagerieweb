ff54b77521f81773b16162e8bc307766cb4ace77
KNG Vladis <nathankala99@gmail.com>
2021-01-27 07:26:01 +0100

    petite correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountcreationComponent } from './pages/accountcreation/accountcreation.component';
-import { AuthenticationComponent } from './pages/authentication/authentication.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccountcreationComponent,
-    AuthenticationComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
