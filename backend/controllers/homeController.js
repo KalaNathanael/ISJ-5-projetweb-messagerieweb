d2cd82696c31048d153b1c575d5c5f9471a112c8
kembou vanel <browndonfe@gmail.com>
2021-01-30 11:33:49 +0100

    ajout d'un utilisateur

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,44 +1,54 @@
 const User=require('../models/user');
 const Contact=require('../models/contact');
 const Message=require('../models/message');
-
+const bcrypt=require('bcrypt');
 
 
 exports.home=(req,res,next)=>{
-
+     console.log(req.body);
     res.json({ message: 'Votre requête a bien été effectue !' });
      next();
 
 
 };
+exports.test=(req,res,next)=>{
+          
+      res.json({message:req.body.name});
+      next();
+};
 
 exports.createUser=(req,res,next)=>{
 
-   const user=new User({
-
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const user=new User({
       username:req.body.username,
       name:req.body.name,
       phone:req.body.phone,
+      password:hash,
       email:req.body.email,
-      password:req.body.password,
       isphoneverified:false,
       isemailverified:false
+    });
 
-   });
-   user.save().then(
-     ()=>{
-         res.status(201).json({
-             message:"user added successfull"
-         });
-     }
 
-   ).catch(
-       (error)=>{
-           res.status(400).json({
-               error:error
-           });
-       }
-   )
+    user.save().then(
+        ()=>{
+            res.status(201).json({
+                message:"user added successfull"
+            });
+        }
+   
+      ).catch(
+          (error)=>{
+              res.status(400).json({
+                  error:error
+              });
+          }
+      )
+
+   }).catch(error => res.status(500).json({ error }));
+   
 
 
 };
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,5 +7,6 @@ const controller=require('../controllers/homeController');
 // ici on met les differentes routes
    router.get('/',controller.home);
    router.post('/add-user',controller.createUser);
+   router.post('/test',controller.test);
 
 module.exports = router;
